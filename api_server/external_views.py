d1f4cea2f008c5a0797af73efee4c7aff975b2f0
Ruming Wang <t-rumwan@microsoft.com>
2020-01-15 19:06:11 +0800

    Remove Roselia-GBP
    Remove unnecessary Roselia-Lyric api.

--- api_server/external_views.py
+++ api_server/external_views.py
@@ -1,4 +1,3 @@
-from RoseliaGBP.view import bp as roselia_gbp_view
 from views.User import user_view
 from views.Comment import comment_view
 from views.Oauth import oauth_view
@@ -12,7 +11,6 @@ from models import database
 from flask_cors import CORS
 
 views = [
-    roselia_gbp_view,
     user_view,
     comment_view,
     oauth_view,
